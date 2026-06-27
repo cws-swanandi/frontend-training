8568d809485e573181702f1c81d6ad969217d2b8
Swanandi Wadghule <cws.swanandi@gmail.com>
2026-06-27 12:28:45 +0530

    Added frontend training project files

--- tr_Day6/navigation_app/src/pages/about.js
+++
@@ -1,6 +0,0 @@
-function About(){
-    return(
-        <p>About us</p>
-    );
-}
-export default About;
--- tr_Day6/navigation_app/src/pages/services.js
+++
@@ -1,6 +0,0 @@
-function Services(){
-    return(
-        <p>Our services</p>
-    );
-}
-export default Services;
